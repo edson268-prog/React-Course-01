dde0131d256030d2f2e7d8d6059d085f258cd638
edson.villacorta <edson26web@gmail.com>
2025-10-28 13:31:46 -0400

    first commit

---
+++ src/bases/01-const-let.ts
@@ -0,0 +1,9 @@
+const firstName = "Edson";
+const lastName = "Ibañez";
+
+let diceNumber = 5;
+diceNumber = 3;
+
+const containsLetterH = lastName.includes("h");
+
+console.log({ containsLetterH, diceNumber, firstName, lastName });
---
+++ src/bases/02-template-string.ts
@@ -0,0 +1,6 @@
+const firstName = "Edson";
+const lastName = "O'Neal";
+
+const fullName = `El nombre es: ${firstName} ${lastName}`;
+
+console.log(fullName);
